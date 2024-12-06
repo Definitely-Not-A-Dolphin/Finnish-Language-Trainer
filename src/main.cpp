b071514c6c568dbd5f79cc0217f23b5d1146e5d0
Definitely Not A Dolphin <derek.verduijn@outlook.com>
2024-12-06 18:30:19 +0100

    we will fix this bs later!

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,20 @@
 CsvParser csv("english_finnish.csv");
 
 std::string getFinnish(std::string wordEnglish);
+std::string getEnglish(std::string wordFinnish);
 
-int main() { 
+    int main() {
   std::cout << getFinnish("nice") << std::endl; 
   
+  std::cout << getEnglish("tähti") << std::endl;
+
+  return 0;
 }
 
 std::string getFinnish(std::string wordEnglish) {
-  for (std::string i : csv.get_mathching_finnish(wordEnglish)) {
-    std::cout << i;
-  }
+  return (csv.get_mathching_finnish(wordEnglish))[0];
+}
+
+std::string getEnglish(std::string wordFinnish) {
+  return (csv.get_mathching_english(wordFinnish));
 }
