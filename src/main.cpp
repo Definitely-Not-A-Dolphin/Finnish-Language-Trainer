13427487b8ab47281d8e68cea1285ae3b10d82e0
Definitely Not A Dolphin <158269354+Definitely-Not-A-Dolphin@users.noreply.github.com>
2024-12-06 12:52:20 +0000

    ill finish this later (pun unintended)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include "parser.hpp"
+#include <string>
 #include <iostream>
+
+string getFinnish(string wordEnglish);
 
 int main() {
   CsvParser csv("english_finnish.csv");
 
   for (std::string i : csv.get_mathching_finnish(
-           "nice")) { // for-loop because there could be more than one
+           "star")) { // for-loop because there could be more than one
                       // translation of a word.
     std::cout << "Match found: " << i << std::endl;
   }
+}
+
+string getFinnish(string wordEnglish){
+  
 }
