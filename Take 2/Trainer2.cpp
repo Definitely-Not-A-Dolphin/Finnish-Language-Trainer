ffdf63e2d1a300f651b316cc34ffab6574808eaf
Definitely Not A Dolphin <158269354+Definitely-Not-A-Dolphin@users.noreply.github.com>
2024-12-09 13:43:52 +0000

    Version 1.2.2 (half)

--- Take 2/Trainer2.cpp
+++ Take 2/Trainer2.cpp
@@ -3,30 +3,30 @@
 #include <list>
 #include <string>
 
-std::ifstream readEngFin("wordsEngFin.txt");
-
 void englishFinnish(bool IP);
-
 void pointerMover(std::ifstream &file, int k);
-
 bool listChecker(std::list<std::string> searchedList, std::string searchedWord);
+void getFinWord(int k);
+
+std::ifstream readEngFin("wordsEngFin.txt");
 
 int maxLine = 50;
 
 bool infinitePractise;
 
-int main() {
+bool randomPractise;
 
+std::list<std::string> noList = {"no",  "No",  "nah", "Nah",
+                                 "nei", "Nei", "nee", "Nee"};
+std::list<std::string> yesList = {"Yes", "yes", "Yeah", "yeah",
+                                  "Yea", "yea", "Ye",   "ye"};
+
+int main() {
 
   while (true) {
     std::string answerIP;
     std::cout << "Do you want to infinite practise? ";
     std::cin >> answerIP;
-
-    std::list<std::string> noList = {"no",  "No",  "nah", "Nah",
-                                     "nei", "Nei", "nee", "Nee"};
-    std::list<std::string> yesList = {"Yes", "yes", "Yeah", "yeah",
-                                      "Yea", "yea", "Ye",   "ye"};
 
     if (listChecker(yesList, answerIP)) {
       infinitePractise = true;
@@ -35,52 +35,64 @@ int main() {
       infinitePractise = false;
       break;
     } else {
-      std::cout << "Sorry, I could not understand that. Please re-enter your answer." << std::endl;
+      std::cout
+          << std::endl
+          << "Sorry, I could not understand that. Please re-enter your answer."
+          << std::endl;
     }
   }
-  
+
+  while (true) {
+    std::string answerRandom;
+    std::cout << "Do you want to randomise the words?? ";
+    std::cin >> answerRandom;
+
+    if (listChecker(yesList, answerRandom)) {
+      randomPractise = true;
+      break;
+    } else if (listChecker(noList, answerRandom)) {
+      randomPractise = false;
+      break;
+    } else {
+      std::cout
+          << std::endl
+          << "Sorry, I could not understand that. Please re-enter your answer."
+          << std::endl;
+    }
+  }
+
   englishFinnish(infinitePractise);
 
   return 0;
 }
 
-void englishFinnish(bool IP) {
+void englishFinnish(bool IP, bool RP) {
+
+  int counter = 0;
 
   int i = 1;
 
-  while (true) {
+  if (RP = false) {
+    while (true) {
 
-    pointerMover(readEngFin, i);
+      getFinWord(i);
 
-    std::string wordEng;
-    getline(readEngFin, wordEng, '|');
+      counter = counter + 1;
 
-    readEngFin.seekg(1, std::ifstream::cur);
-
-    std::string wordFin;
-    getline(readEngFin, wordFin, '|');
-
-    std::string wordFinInput;
-    std::cout << "What is the Finnish word for " << wordEng << "? ";
-    std::cin >> wordFinInput;
-
-    if (wordFinInput == wordFin) {
-      std::cout << "Yes! :)" << std::endl;
-    } else {
-      std::cout << "No :(" << std::endl;
-    };
-
-    i = i + 1;
-
-    if (IP = false) {
-      break;
-    } else {
-      i = 1;
+      if (i > maxLine) {
+        if (IP = false) {
+          break;
+        } else {
+          i = 1;
+        }
+      }
     }
   }
 }
 
 void pointerMover(std::ifstream &file, int k) {
+
+  // Special thanks to Elses/Menium for writing this part!
 
   file.clear();
   file.seekg(0, std::ios::beg);
@@ -105,4 +117,26 @@ bool listChecker(std::list<std::string> searchedList,
   };
 
   return result;
+}
+
+void getFinWord(int k) {
+  pointerMover(readEngFin, k);
+
+  std::string wordEng;
+  getline(readEngFin, wordEng, '|');
+
+  readEngFin.seekg(1, std::ifstream::cur);
+
+  std::string wordFin;
+  getline(readEngFin, wordFin, '|');
+
+  std::string wordFinInput;
+  std::cout << "What is the Finnish word for " << wordEng << "? " << std::endl;
+  std::cin >> wordFinInput;
+
+  if (wordFinInput == wordFin) {
+    std::cout << "Yes! :)" << std::endl;
+  } else {
+    std::cout << "No :(" << std::endl;
+  };
 }
